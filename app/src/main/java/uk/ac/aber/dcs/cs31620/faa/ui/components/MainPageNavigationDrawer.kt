f11b6a31132530857151b8d39bfc8ee99e585991
Ava Wei <weizirou6688@gmail.com>
2025-06-10 16:12:59 +0100

    Add navigation drawer

---
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/faa/ui/components/MainPageNavigationDrawer.kt
@@ -0,0 +1,130 @@
+package uk.ac.aber.dcs.cs31620.faa.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Help
+import androidx.compose.material.icons.automirrored.filled.Login
+import androidx.compose.material.icons.filled.Feedback
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import uk.ac.aber.dcs.cs31620.faa.R
+import uk.ac.aber.dcs.cs31620.faa.ui.theme.FAATheme
+
+@Composable
+fun MainPageNavigationDrawer(
+    navController: NavController,
+    drawerState: DrawerState,
+    closeDrawer: () -> Unit = {},
+    content: @Composable () -> Unit = {}
+) {
+    var selectedItem by
+    rememberSaveable { mutableStateOf("") }
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            // Drawer content goes here
+            ModalDrawerSheet {
+                Column(
+                    horizontalAlignment =
+                        Alignment.CenterHorizontally,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    // Add image
+                    // SLIDE 9
+                    Image(
+                        modifier = Modifier
+                            .size(200.dp)
+                            .padding(bottom = 16.dp, top = 16.dp),
+                        painter = painterResource(id = R.drawable.kitten_small),
+                        contentDescription =
+                            stringResource(R.string.small_kitten),
+                        contentScale = ContentScale.Crop
+                    )
+                    // Add NavigationDrawerItems
+                    // SLIDES 10 and 11
+                    DisplayNavItem(
+                        imageVector = Icons.AutoMirrored.Filled.Login,
+                        description = stringResource(R.string.login),
+                        selectedItem = selectedItem,
+                        updateSelected = { selectedItem = it },
+                        doClick = { } // Navigate to some login screen
+                    )
+                    DisplayNavItem(
+                        imageVector = Icons.AutoMirrored.Filled.Help,
+                        description = stringResource(R.string.help),
+                        selectedItem = selectedItem,
+                        updateSelected = { selectedItem = it },
+                    )
+                    DisplayNavItem(
+                        imageVector = Icons.Default.Feedback,
+                        description = stringResource(R.string.feedback),
+                        selectedItem = selectedItem,
+                        updateSelected = { selectedItem = it },
+                    )
+
+                }
+            }
+        },
+        // Page content here
+        content = content
+    )
+}
+
+@Composable
+private fun DisplayNavItem(
+    imageVector: ImageVector,
+    description: String,
+    selectedItem: String,
+    updateSelected: (String) -> Unit = {},
+    doClick: () -> Unit = {}
+) {
+    NavigationDrawerItem(
+        icon = {
+            Icon(
+                imageVector = imageVector,
+                contentDescription = description
+            )
+        },
+        label = { Text(description) },
+        selected = selectedItem == description,
+        onClick = {
+            updateSelected(description)
+            doClick()
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun MainPageNavigationDrawerPreview() {
+    FAATheme(dynamicColor = false) {
+        val navController = rememberNavController()
+        val drawerState = rememberDrawerState(initialValue = DrawerValue.Open)
+        MainPageNavigationDrawer(navController, drawerState)
+    }
+}
--- app/src/main/java/uk/ac/aber/dcs/cs31620/faa/ui/components/TopLevelScaffold.kt
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/faa/ui/components/TopLevelScaffold.kt
@@ -1,26 +1,52 @@
 package uk.ac.aber.dcs.cs31620.faa.ui.components
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
-import androidx.navigation.NavHostController
+import kotlinx.coroutines.launch
+import uk.ac.aber.dcs.cs31620.faa.ui.theme.FAATheme
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun TopLevelScaffold(
     navController: NavController,
     pageContent:
     @Composable (innerPadding: PaddingValues) -> Unit = {}
-){
-    Scaffold(
-        topBar = {
-            MainPageTopAppBar()
-        },
-        bottomBar = {
-            MainPageNavigationBar(navController)
-        },
-        content = { innerPadding -> pageContent(innerPadding) }
-    )
+) {
+    val drawerState =
+        rememberDrawerState(initialValue = DrawerValue.Closed)
+    val coroutineScope = rememberCoroutineScope()
+
+    MainPageNavigationDrawer(
+        navController = navController,
+        drawerState = drawerState,
+        closeDrawer = {
+            coroutineScope.launch {
+                drawerState.close()
+            }
+        }
+    ) {
+
+        Scaffold(
+            topBar = {
+                MainPageTopAppBar(onClick = {
+                    coroutineScope.launch {
+                        if (drawerState.isOpen) {
+                            drawerState.close()
+                        } else {
+                            drawerState.open()
+                        }
+                    }
+                })
+            },
+            bottomBar = {
+                MainPageNavigationBar(navController)
+            },
+            content = { innerPadding -> pageContent(innerPadding) }
+        )
+    }
 }
