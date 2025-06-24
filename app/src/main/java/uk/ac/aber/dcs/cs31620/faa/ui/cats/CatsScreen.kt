eea2ba1f4de3ed3278c8629aa92241d35336d8d1
Ava Wei <weizirou6688@gmail.com>
2025-06-24 10:32:28 +0100

    Add cats screen with search area

--- app/src/main/java/uk/ac/aber/dcs/cs31620/faa/ui/cats/CatsScreen.kt
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/faa/ui/cats/CatsScreen.kt
@@ -1,15 +1,32 @@
 package uk.ac.aber.dcs.cs31620.faa.ui.cats
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import uk.ac.aber.dcs.cs31620.faa.R
+import uk.ac.aber.dcs.cs31620.faa.ui.components.ButtonSpinner
+import uk.ac.aber.dcs.cs31620.faa.ui.components.SearchArea
 import uk.ac.aber.dcs.cs31620.faa.ui.components.TopLevelScaffold
 import uk.ac.aber.dcs.cs31620.faa.ui.theme.FAATheme
 /**
@@ -24,14 +41,29 @@ fun CatsScreen(
     TopLevelScaffold(
         navController = navController
     ) { innerPadding ->
-        Surface(
+        Column(
             modifier = Modifier
                 .padding(innerPadding)
                 .fillMaxSize()
         ) {
-            Text(
-                text = "Cats Screen",
-                modifier = Modifier.padding(start = 8.dp)
+
+            val breedList = stringArrayResource(id = R.array.breed_array).toList()
+            val genderList = stringArrayResource(id = R.array.gender_array).toList()
+            val ageList = stringArrayResource(id = R.array.age_range_array).toList()
+            var selectedBreed by remember { mutableStateOf(breedList[0]) }
+            var selectedGender by remember { mutableStateOf(genderList[0]) }
+            var selectedAge by remember { mutableStateOf(ageList[0]) }
+            var proximity by remember { mutableIntStateOf(10) }
+
+            SearchArea(
+                breedList = breedList,
+                updateBreed = { selectedBreed = it },
+                genderList = genderList,
+                updateGender = { selectedGender = it },
+                ageList = ageList,
+                updateAge = { selectedAge = it },
+                proximity = proximity,
+                updateProximity = { proximity = it }
             )
         }
     }
---
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/faa/ui/components/SearchArea.kt
@@ -0,0 +1,93 @@
+package uk.ac.aber.dcs.cs31620.faa.ui.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import uk.ac.aber.dcs.cs31620.faa.R
+
+/**
+ * Displays a search area consisting of two rows of filter
+ * dropdown buttons.
+ * Row 1 has filters for breed and gender.
+ * Row 2 has a filter for age and an outline button for the search distance.
+ * @param modifier To configure the search Card
+ * @param breedList The list of breeds to display in the dropdown
+ * @param updateBreed The lambda to run if a breed is selected
+ * @param genderList The list of genders to display in the dropdown
+ * @param updateGender The lambda to run if a gender is selected
+ * @param ageList The list of age ranges to display in the dropdown
+ * @param updateAge The lambda to run if an age range is selected
+ * @param proximity The distance an adopter is willing to travel
+ * @param updateProximity The lambda to run if the distance is changed
+ */
+@Composable
+fun SearchArea(
+    modifier: Modifier = Modifier,
+    breedList: List<String>,
+    updateBreed: (String)-> Unit,
+    genderList: List<String>,
+    updateGender: (String)-> Unit,
+    ageList: List<String>,
+    updateAge: (String)-> Unit,
+    proximity: Int,
+    updateProximity: (Int)-> Unit
+){
+    Card(
+        shape = RectangleShape,
+        elevation =
+            CardDefaults.cardElevation(defaultElevation = 4.dp),
+        modifier = modifier
+    ) {
+        Row {
+            ButtonSpinner(
+                items = breedList,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 8.dp, top = 8.dp, end = 8.dp),
+                itemClick = updateBreed
+            )
+
+            ButtonSpinner(
+                items = genderList,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(top = 8.dp, end = 8.dp),
+                itemClick = updateGender
+            )
+        }
+
+        // Second row
+        Row {
+            ButtonSpinner(
+                items = ageList,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 8.dp, end = 8.dp, bottom = 8.dp),
+                itemClick = updateAge
+            )
+
+            OutlinedButton(
+                onClick = {
+                    // To be defined in section 2
+                },
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(end = 8.dp, bottom = 8.dp)
+            ) {
+                Text(
+                    text = stringResource(id = R.string.distance, proximity),
+                    fontSize = 16.sp
+                )
+            }
+        }
+    }
+}
