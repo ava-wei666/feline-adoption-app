be4f902db38193e6e5c7010f2347eec8cfb026b6
Ava Wei <weizirou6688@gmail.com>
2025-06-25 07:59:30 +0100

    Add cats grid with reusable CatCard component

--- app/src/main/java/uk/ac/aber/dcs/cs31620/faa/ui/cats/CatsScreen.kt
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/faa/ui/cats/CatsScreen.kt
@@ -1,14 +1,12 @@
 package uk.ac.aber.dcs.cs31620.faa.ui.cats
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -16,22 +14,22 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringArrayResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import uk.ac.aber.dcs.cs31620.faa.R
-import uk.ac.aber.dcs.cs31620.faa.ui.components.ButtonSpinner
+import uk.ac.aber.dcs.cs31620.faa.model.cats
+import uk.ac.aber.dcs.cs31620.faa.ui.components.CatCard
 import uk.ac.aber.dcs.cs31620.faa.ui.components.SearchArea
 import uk.ac.aber.dcs.cs31620.faa.ui.components.TopLevelScaffold
 import uk.ac.aber.dcs.cs31620.faa.ui.theme.FAATheme
+
 /**
  * Represents the cats screen. For this version we only have a
- * top app bar, navigation bar and empty content.
+ * top app bar, navigation bar, search area and a list of cats.
  * @author Chris Loftus
  */
 @Composable
@@ -55,6 +53,8 @@ fun CatsScreen(
             var selectedAge by remember { mutableStateOf(ageList[0]) }
             var proximity by remember { mutableIntStateOf(10) }
 
+            val context = LocalContext.current
+
             SearchArea(
                 breedList = breedList,
                 updateBreed = { selectedBreed = it },
@@ -65,6 +65,31 @@ fun CatsScreen(
                 proximity = proximity,
                 updateProximity = { proximity = it }
             )
+
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 4.dp, bottom = 4.dp)
+            ) {
+
+                items(cats) {
+                    CatCard(
+                        cat = it,
+                        modifier = Modifier
+                            .padding(end = 4.dp, top = 4.dp),
+                        selectAction = {cat ->
+                            Toast.makeText(context, "Selected ${cat.name}",
+                                Toast.LENGTH_LONG).show()
+                        },
+                        deleteAction = {cat ->
+                            Toast.makeText(context, "Delete ${cat.name}",
+                                Toast.LENGTH_LONG).show()
+                        }
+                    )
+                }
+            }
+
         }
     }
 }
